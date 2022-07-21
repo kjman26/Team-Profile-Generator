fb9357a9fba478a85f79c9c175f96c3a8c4d6df7
Chris <kjman26@gmail.com>
2022-07-21 09:33:40 -0700

    roles

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee")
+
+const Engineer extends Employee {
+    constructor(name, id, email, GitHub){
+        super(name, id, email)
+        
+        this.GitHub = GitHub
+  }  
+
+  getGithub(){
+    return this.GitHub
+  }
+
+  getRole(){
+    return "Engineer"
+  }
+}
+
+module.exports = Engineer
